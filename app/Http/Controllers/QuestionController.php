486b1d1a115f0c9ecd02e4aec881a247cdea7435
Crxwler <loco7139@gmail.com>
2021-12-19 19:28:57 -0600

    route fixed

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -34,7 +34,7 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return redirect()->route('estados.index');
     }
 
     /**
